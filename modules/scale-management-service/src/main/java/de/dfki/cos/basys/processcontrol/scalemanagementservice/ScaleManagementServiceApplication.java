c81d506ac7f5d8d98c81381941e75559307bdd39
jomo02 <jonas.mohr@dfki.de>
2023-09-20 14:05:37 +0200

    Add scale management service refs i40/administration#229

---
+++ modules/scale-management-service/src/main/java/de/dfki/cos/basys/processcontrol/scalemanagementservice/ScaleManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package de.dfki.cos.basys.processcontrol.scalemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaleManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScaleManagementServiceApplication.class, args);
+	}
+
+}
---
+++ modules/scale-management-service/src/main/java/de/dfki/cos/basys/processcontrol/scalemanagementservice/services/ScaleManager.java
@@ -0,0 +1,53 @@
+package de.dfki.cos.basys.processcontrol.scalemanagementservice.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.basyx.aas.manager.api.IAssetAdministrationShellManager;
+import de.dfki.cos.basys.aas.registry.model.*;
+import org.eclipse.basyx.submodel.metamodel.api.ISubmodel;
+import org.eclipse.basyx.submodel.metamodel.api.identifier.IdentifierType;
+import org.eclipse.basyx.submodel.metamodel.api.submodelelement.ISubmodelElementCollection;
+import org.eclipse.basyx.submodel.metamodel.api.submodelelement.dataelement.IProperty;
+import org.eclipse.basyx.submodel.metamodel.map.identifier.Identifier;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Service
+@Slf4j
+public class ScaleManager {
+
+    @Autowired
+    private AasRegistryQueries aasRegistryServices;
+
+    @Autowired
+    private IAssetAdministrationShellManager aasManager;
+
+    @Value("${basys.semanticIds.processSubmodel}")
+    private String processSubmodelSemanticId;
+
+    @Value("${basys.semanticIds.idShort}")
+    private String idShort;
+
+    @PostConstruct
+    public void initialize() {
+        log.info("Started ScaleManagementService");
+
+        AssetAdministrationShellDescriptor aasDescriptor = aasRegistryServices.searchAasDescriptorByIdShort(idShort);
+        Key instanceKey = new Key().type(KeyTypes.CONCEPTDESCRIPTION).value(processSubmodelSemanticId);
+
+        var opt = aasDescriptor.getSubmodelDescriptors().stream().filter(smd -> smd.getSemanticId().getKeys().contains(instanceKey)).findFirst();
+        opt.ifPresent(smDescriptor -> {
+            //Retrieve Submodel
+            ISubmodel instanceSubmodel = aasManager.retrieveSubmodel(new Identifier(IdentifierType.CUSTOM, aasDescriptor.getIdentification()), new Identifier(IdentifierType.CUSTOM, smDescriptor.getIdentification()));
+            ISubmodelElementCollection processesCollection = (ISubmodelElementCollection) instanceSubmodel.getSubmodelElement("Processes");
+            ISubmodelElementCollection manufacturingProcess = (ISubmodelElementCollection) processesCollection.getSubmodelElement("ManufacturingBoxedRaspberryPi");
+            IProperty maxDuration = manufacturingProcess.getProperties().get("MaxDuration");
+            log.info("MaxDuration: {}", maxDuration.getValue());
+        });
+
+    }
+
+
+}
