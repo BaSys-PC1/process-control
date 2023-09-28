3f928d0950d40ff76c0403b6296e686dcc3fad94
jomo02 <jonas.mohr@dfki.de>
2023-09-28 14:13:35 +0200

    Send correct values, check for 404 status code, set Eureka server from outside refs i40/administration#229

--- modules/scale-management-service/src/main/java/de/dfki/cos/basys/processcontrol/scalemanagementservice/services/ServiceRegistryManager.java
+++ modules/scale-management-service/src/main/java/de/dfki/cos/basys/processcontrol/scalemanagementservice/services/ServiceRegistryManager.java
@@ -8,13 +8,16 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import javax.json.Json;
 import javax.json.JsonObject;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Send configuration (retrieved from AAS) to scale-control as soon as it comes online.
@@ -42,9 +45,20 @@ public class ServiceRegistryManager {
 
     @Scheduled(fixedRate = 5000)
     public void checkAvailability() {
-        String url = eurekaConnectionString + "/v2/apps/scale-controller/scale-pc";
-        InstanceWrapper instance = this.restTemplate.getForObject(url, InstanceWrapper.class);
-        assert instance != null;
+        String url = this.eurekaConnectionString + "/v2/apps/scale-controller/scale-pc";
+        InstanceWrapper instance = null;
+
+        try {
+            instance = this.restTemplate.getForObject(url, InstanceWrapper.class);
+        }
+        catch (HttpClientErrorException ex) {
+            if (ex.getStatusCode() != HttpStatus.NOT_FOUND) {
+                throw ex;
+            }
+        }
+
+        if (instance == null) return;
+
         if (instance.getInstance().getStatus().equals("UP") && !this.isUp) {
             this.isUp = true;
             this.sendConfig();
@@ -58,16 +72,20 @@ public class ServiceRegistryManager {
         // TODO: Retrieve from AAS
         JsonObject tarePayload = Json.createObjectBuilder()
                 .add("channel", 1)
-                .add("value", 0.1)
+                .add("value", 0.043855) // box weight
                 .build();
         JsonObject refPiecesPayload = Json.createObjectBuilder()
                 .add("channel", 1)
-                .add("pieces", 3)
+                .add("pieces", 6)// 0.0014 / piece with High precision
                 .build();
         try {
+            TimeUnit.SECONDS.sleep(1);
+            mqttClient.publish("scale/startMeasurement", new MqttMessage("{}".getBytes()));
+            TimeUnit.SECONDS.sleep(1);
             mqttClient.publish("scale/tare", new MqttMessage(tarePayload.toString().getBytes()));
+            TimeUnit.SECONDS.sleep(1);
             mqttClient.publish("scale/refPieces", new MqttMessage(refPiecesPayload.toString().getBytes()));
-        } catch (MqttException e) {
+        } catch (MqttException | InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
