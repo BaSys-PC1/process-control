a3a2c8626109b89d5537c8d2c3a87d4eca86f1b1
jomo02 <jonas.mohr@dfki.de>
2023-10-17 15:28:35 +0200

    Consume WGS specific Kafka topics refs i40/administration#231

--- modules/wgs-management-service/src/main/java/de/dfki/cos/basys/processcontrol/wgsmanagementservice/services/KafkaService.java
+++ modules/wgs-management-service/src/main/java/de/dfki/cos/basys/processcontrol/wgsmanagementservice/services/KafkaService.java
@@ -1,7 +1,9 @@
 package de.dfki.cos.basys.processcontrol.wgsmanagementservice.services;
 
-import de.dfki.cos.basys.aas.registry.events.RegistryEvent;
+import de.dfki.cos.basys.processcontrol.model.Notification;
+import de.dfki.cos.basys.processcontrol.model.StepChange;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
@@ -12,16 +14,30 @@ import java.util.function.Consumer;
 @Slf4j
 public class KafkaService {
 
+    @Autowired
     private StreamBridge streamBridge;
 
     @Bean
-    public Consumer<RegistryEvent> aasRegistryUpdates() {
-        return this::handleAasRegistryUpdates;
+    public Consumer<Notification> notificationUpdates() {
+        return this::handleNotificationUpdates;
     }
 
-    private void handleAasRegistryUpdates(RegistryEvent event) {
-        log.info("new AAS registry event: " + event.getType());
+    private void handleNotificationUpdates(Notification notification) {
+        log.info("new notification of type {} and show {}", notification.getType(), notification.getShow());
 
-        //streamBridge.send("aasRegistryUpdates", event);
+//        if (notification.getShow()) {
+//            notification.setShow(false);
+//            streamBridge.send("notification", notification);
+//        }
+
+    }
+
+    @Bean
+    public Consumer<StepChange> stepChangeUpdates() {
+        return this::handleStepChangeUpdates;
+    }
+
+    private void handleStepChangeUpdates(StepChange stepChange) {
+        log.info("new step change with id {}", stepChange.getWorkstepId());
     }
 }
